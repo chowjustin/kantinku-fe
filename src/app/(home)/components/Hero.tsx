b622043d5a9a0c0f0119f650949e3b23af1361ae
ammrbhlwn <muammar.bahalwan@gmail.com>
2025-05-01 21:24:26 +0700

    feat: add hero section

---
+++ src/app/(home)/components/Hero.tsx
@@ -0,0 +1,37 @@
+import Button from "@/components/buttons/Button";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="relative w-full min-h-[80vh] flex items-center">
+      {/* Background */}
+      <Image
+        src="/images/BackgroundHero.png"
+        alt="Hero Background"
+        fill
+        className="absolute inset-0 object-cover z-0"
+      />
+
+      {/* Gradient Shadow */}
+      <div className="absolute inset-0 z-10 bg-gradient-to-r from-white/100 via-white/80 to-transparent" />
+
+      {/* Text Banner */}
+      <div className="relative z-20 pl-20 flex flex-col gap-5">
+        <h1 className="text-5xl font-bold max-w-xs leading-[64px]">
+          Quick Meals, Happy Deals!
+        </h1>
+        <p className="text-2xl max-w-md">
+          No more waiting in line, your canteen favorites are just a tap away!
+        </p>
+        <Button
+          size="lg"
+          className="w-fit bg-[#243E80] hover:bg-[#243E80] border-none hover:shadow-[#243E80] transition-all duration-300 p-4"
+        >
+          Explore Menu
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/layouts/Navbar";
+import Footer from "@/layouts/Footer";
+import Hero from "./components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
