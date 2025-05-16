eb7de215302c14be4126d83eba786adcfd48f3e5
Justin Chow <justin.chow05@gmail.com>
2025-05-16 20:03:36 +0700

    fix: fix navbar menus anomaly

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,8 +5,11 @@ import Hero from "./components/Hero";
 import MenuSection from "./components/MenuSection";
 import Footer from "@/layouts/Footer";
 import CartFooter from "@/layouts/CartFooter";
+import withAuth from "@/components/hoc/withAuth";
 
-export default function Home() {
+export default withAuth(Home, "public");
+
+function Home() {
   return (
     <>
       <Navbar />
